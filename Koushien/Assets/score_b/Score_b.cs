39610e361d85709eba3f7647e5fe55143eed8624
ユウ <youjiacheng9@gmail.com>
2024-06-18 10:38:15 +0900

    修正

--- Koushien/Assets/score_b/Score_b.cs
+++ Koushien/Assets/score_b/Score_b.cs
@@ -1,18 +1,59 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Score_b : MonoBehaviour
+public class Score : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void ApplyEffect(int ballType1, int ballType2, int totalPower)
     {
-        
+        //
+        if ((ballType1 == 0 && ballType2 == 1) || (ballType1 == 1 && ballType2 == 0))
+        {
+            //
+            IncreaseEnemyDamage(totalPower);
+        }
+        else if ((ballType1 == 1 && ballType2 == 3) || (ballType1 == 3 && ballType2 == 1))
+        {
+            //
+            ReducePlayerDamage(totalPower);
+        }
+        else if ((ballType1 == 0 && ballType2 == 3) || (ballType1 == 3 && ballType2 == 0))
+        {
+            //
+            ResetDamageCounter(totalPower);
+        }
+        else
+        {
+            //
+            DealDamageToEnemy(totalPower);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void IncreaseEnemyDamage(int totalPower)
     {
-        
+        //
+        Debug.Log($"Increase enemy damage by {totalPower}x for 30 seconds");
+        //
+    }
+
+    private static void ReducePlayerDamage(int totalPower)
+    {
+        //
+        Debug.Log($"Reduce player damage by {totalPower} for 30 seconds");
+        //
+    }
+
+    private static void ResetDamageCounter(int totalPower)
+    {
+        //
+        Debug.Log($"Reset damage counter by {totalPower * 3} seconds");
+        //
+    }
+
+    private static void DealDamageToEnemy(int totalPower)
+    {
+        // 
+        Debug.Log($"Deal {totalPower} damage to enemy");
+        //
     }
 }
