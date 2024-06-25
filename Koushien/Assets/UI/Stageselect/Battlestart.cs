1910fef9214000b3e1842705f284e4ccbaa50209
matsumoto-2022 <116060216+matsumoto-2022@users.noreply.github.com>
2024-06-25 09:39:41 +0900

    関数の統合
    1文程度の関数を一つのスクリプトにまとめました

--- Koushien/Assets/UI/Stageselect/Battlestart.cs
+++ Koushien/Assets/UI/Stageselect/Battlestart.cs
@@ -1,13 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Battlestart : MonoBehaviour
 {
+    public Button stage1;
+    public Button stage2;
+    public Button stage3;
+    public Button title;
+
+    private void Start()
+    {
+        stage1.onClick.AddListener(() => ButtonClicked("s1"));
+        stage2.onClick.AddListener(() => ButtonClicked("s1"));
+        stage3.onClick.AddListener(() => ButtonClicked("s1"));
+        title.onClick.AddListener(() => ButtonClicked("ti"));
+
+    }
+
+    public void ButtonClicked(string buttonname)
+    {
+        switch (buttonname)
+        {
+            case"s1":
+                //ここにステージ１の敵設定
+
+                LoadBattle();
+                break;
+            case "s2":
+                //ここにステージ2の敵設定
+
+                LoadBattle();
+                break;
+            case "s3":
+                //ここにステージ3の敵設定
+
+                LoadBattle();
+                break;
+            case "ti":
+                LoadTitle();
+                break;
+        }
+    }
+
     public void LoadBattle()
     {
         //タイトルのシーンをロード
         SceneManager.LoadScene("Battle");
+    }
+    public void LoadTitle()
+    {
+        //タイトルのシーンをロード
+        SceneManager.LoadScene("Title");
     }
 }
